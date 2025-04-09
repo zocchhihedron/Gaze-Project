527f070d56225184fede68dfc6dd8e408b14b867
Zofia Lukasiewicz <zofia2670@gmail.com>
2025-04-09 17:59:35 +0200

    File update

---
+++ Gaze-Project-Main/TobiiPro.SDK.Python.Windows_2.1.0.1/64/tobiiresearch/implementation/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ("DisplayArea", "Errors", "ExternalSignalData", "EyeImageData", "EyeTracker", "EyeOpennessData",
+           "GazeData", "License", "_LogEntry", "Notifications", "ScreenBasedCalibration", "StreamErrorData",
+           "TimeSynchronizationData", "UserPositionGuide",
+           "Calibration", "StreamErrorData", "TimeSynchronizationData",
+           "ScreenBasedCalibration", "ScreenBasedMonocularCalibration")
