f85b3ff01f6b66b61d2a4b1a29199db3c6e7149b
Zofia Lukasiewicz <zofia2670@gmail.com>
2025-04-13 13:01:40 +0200

    Fixing import

--- Gaze-Project-Main/Gaze/src/main/kotlin/furhatos/app/gaze/dataImport/JSONimport.kt
+++ Gaze-Project-Main/Gaze/src/main/kotlin/furhatos/app/gaze/dataImport/JSONimport.kt
@@ -4,17 +4,22 @@
 package furhatos.app.gaze.dataImport
 import com.google.gson.Gson //Parsing a JSON file
 import com.google.gson.reflect.TypeToken  //Specify deserialization into an array
+import java.io.InputStreamReader
 
 data class GazeSample(val gaze_duration:Double, val gaze_left_eye:Double, val gaze_right_eye:Double)
 
 fun deserialize(): List<GazeSample> {
-    //Reads the file as text
-    val jsonString = File("datafile_name.json").readText()
+    //Accessing the JSON file from the resources folder
+    val inputStream = GazeSample::class.java.getResourceAsStream("/datafile_name.json")
+        ?: throw IllegalArgumentException("Resource not found: datafile_name.json")
+
+    // Read the stream as a text using InputStreamReader
+    val reader = InputStreamReader(inputStream)
 
     //Deserializes into an array of GazeSample objects
     val gson = Gson()
     val type = object : TypeToken<Array<GazeSample>>() {}.type
-    return gson.fromJson(jsonString, type)
+    return gson.fromJson(reader, type)
 }
 
 val gazeSamples = deserialize()
